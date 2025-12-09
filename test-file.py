ed9c3772031c5a4a8d5ba50134fb769b319c0d17
shuaib-debug <kareemvictor147@gmail.com>
2025-12-09 10:04:18 +0300

    a calc

---
+++ test-file.py
@@ -0,0 +1,22 @@
+# Simple Calculator
+
+print("Simple Calculator")
+print("------------------")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print("Result:", num1 + num2)
+elif operator == "-":
+    print("Result:", num1 - num2)
+elif operator == "*":
+    print("Result:", num1 * num2)
+elif operator == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero")
+else:
+    print("Invalid operator")
